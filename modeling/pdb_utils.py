a1b820cba9453b6a7386bd95ff6b13e4af62959c
Xiao Wang <wang3702@purdue.edu>
2023-08-23 13:47:55 -0400

    fix pdb to cif bugs

--- modeling/pdb_utils.py
+++ modeling/pdb_utils.py
@@ -124,6 +124,21 @@ def rename_chains_cif(pdb_file,  new_chain_id,cif_file):
             z=float(line[46:54])
             line = line[:21] + new_chain_id + line[22:30]+" %.3f %.3f %.3f "%(x,y,z)+line[54:]
             atom_serial += 1
+            new_line=""
+            new_line += line[:4]+"\t"
+            new_line += line[6:11]+"\t"
+            new_line += line[12:16]+"\t"
+            new_line += line[16]+"\t"
+            new_line += line[17:20]+"\t"
+            new_line += line[21]+"\t"
+            new_line += line[22:26]+"\t"
+            new_line += line[26]+"\t"
+            new_line += line[30:38]+"\t"
+            new_line += line[38:46]+"\t"
+            new_line += line[46:54]+"\t"
+            new_line += line[54:60]+"\t"
+            new_line += line[60:66]+"\t"
+            new_line += line[66:]#include \n
             cif_lines.append(line)
 
         # if line.startswith('ENDMDL'):
