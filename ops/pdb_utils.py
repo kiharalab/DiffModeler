dc446bbda3d5cd0fbe0e37eaf95b6b33c1b92a01
Xiao Wang <wang3702@purdue.edu>
2023-09-13 23:11:04 -0400

    a 2digit chain id bug in cif2pdb script

--- ops/pdb_utils.py
+++ ops/pdb_utils.py
@@ -345,6 +345,18 @@ def cif2pdb(input_cif_path,final_pdb_path):
     x_ids = block_list.index("_atom_site.Cartn_x")
     y_ids = block_list.index("_atom_site.Cartn_y")
     z_ids = block_list.index("_atom_site.Cartn_z")
+    tmp_chain_list=[chr(i) for i in range(ord('A'), ord('Z') + 1)]  # uppercase letters
+    tmp_chain_list.extend([chr(i) for i in range(ord('a'), ord('z') + 1)])  # lowercase letters
+    #pre filter chain name list
+    with open(input_cif_path,'r') as rfile:
+        for line in rfile:
+            if len(line)>4 and line[:4]=="ATOM":
+                split_info=line.strip("\n").split()
+                current_chain = split_info[chain_ids]
+                if current_chain in tmp_chain_list:
+                    tmp_chain_list.remove(current_chain)
+    print("remain waiting assign chain number %d"%len(tmp_chain_list))
+    chain_map_dict = {}
     with open(input_cif_path,'r') as rfile:
         with open(final_pdb_path,'w') as wfile:
 
@@ -359,6 +371,13 @@ def cif2pdb(input_cif_path,final_pdb_path):
                     current_x = float(split_info[x_ids])
                     current_y = float(split_info[y_ids])
                     current_z = float(split_info[z_ids])
+                    if len(current_chain)>1: #replace with a temporary id
+                        if current_chain in chain_map_dict:
+                            current_chain = chain_map_dict[current_chain]
+                        else:
+                            remain_select_list=[x for x in tmp_chain_list if x not in list(chain_map_dict.values())]
+                            chain_map_dict[current_chain]=remain_select_list[0]
+                            current_chain = chain_map_dict[current_chain]
                     wline=""
                     wline += "ATOM%7d %-4s %3s%2s%4d    " % (current_atom_index, current_atom_name,
                                                              current_res_name, current_chain,current_res_index)
