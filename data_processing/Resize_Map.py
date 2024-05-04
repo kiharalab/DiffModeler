eb4017699412d1b6c6b4feb647cae7bf395ed21e
Han Zhu <zhu773@purdue.edu>
2024-05-04 01:58:27 -0400

    make sure it works for anisotropic voxel sizes

--- data_processing/Resize_Map.py
+++ data_processing/Resize_Map.py
@@ -9,29 +9,39 @@ def my_reform_1a(input_mrc, output_mrc, use_gpu=False):
     with torch.no_grad() and torch.cuda.amp.autocast(enabled=use_gpu):
 
         with mrcfile.open(input_mrc, permissive=True) as orig_map:
-            voxel_size = np.array([orig_map.voxel_size.x, orig_map.voxel_size.y, orig_map.voxel_size.z])
 
-            # orig_data = torch.from_numpy(orig_map.data.copy().transpose((2, 1, 0))).unsqueeze(0).unsqueeze(0)
+            orig_voxel_size = np.array([orig_map.voxel_size.x, orig_map.voxel_size.y, orig_map.voxel_size.z])
+
             orig_data = torch.from_numpy(orig_map.data.copy()).unsqueeze(0).unsqueeze(0)
 
             orig_data = orig_data.cuda() if use_gpu else orig_data
 
-            print("Previous shape: ", orig_data.shape)
+            print("Previous shape (ZYX): ", orig_data.shape)
+            print("Previous voxel size (ZXY): ", np.array([orig_map.voxel_size.z, orig_map.voxel_size.y, orig_map.voxel_size.x]))
+
             # orig = np.array([orig_map.header.origin.x, orig_map.header.origin.y, orig_map.header.origin.z])
 
-            new_grid_size = np.floor(np.array(orig_data.shape[2:]) * voxel_size).astype(np.int32)
+            new_grid_size = np.array(orig_data.shape[2:]) * np.array([orig_map.voxel_size.z, orig_map.voxel_size.y, orig_map.voxel_size.x])
+
+            # print("New grid size (ZYX): ", new_grid_size)
+
+            new_grid_size = np.floor(new_grid_size).astype(np.int32) # ZYX
+
+            print("New grid size (ZYX): ", new_grid_size)
 
             # for compatibility with torch 1.9 and below
             kwargs = {"indexing": "ij"} if (torch.__version__.split(".")[0] >= "2" or torch.__version__.split(".")[0] >= "10") else {}
 
+            z = torch.arange(0, new_grid_size[0], device="cuda" if use_gpu else "cpu") / orig_voxel_size[2] / (orig_data.shape[2] - 1) * 2 - 1
+            y = torch.arange(0, new_grid_size[1], device="cuda" if use_gpu else "cpu") / orig_voxel_size[1] / (orig_data.shape[3] - 1) * 2 - 1
+            x = torch.arange(0, new_grid_size[2], device="cuda" if use_gpu else "cpu") / orig_voxel_size[0] / (orig_data.shape[4] - 1) * 2 - 1
+
+            # noinspection PyArgumentList
             new_grid = torch.stack(
                 torch.meshgrid(
-                    torch.arange(0, new_grid_size[2], device="cuda" if use_gpu else "cpu") / voxel_size[2] / (orig_data.shape[4] - 1) * 2
-                    - 1,
-                    torch.arange(0, new_grid_size[1], device="cuda" if use_gpu else "cpu") / voxel_size[1] / (orig_data.shape[3] - 1) * 2
-                    - 1,
-                    torch.arange(0, new_grid_size[0], device="cuda" if use_gpu else "cpu") / voxel_size[0] / (orig_data.shape[2] - 1) * 2
-                    - 1,
+                    x,
+                    y,
+                    z,
                     **kwargs
                 ),
                 dim=-1,
@@ -41,8 +51,8 @@ def my_reform_1a(input_mrc, output_mrc, use_gpu=False):
             new_data = F.grid_sample(orig_data, new_grid, mode="bilinear", align_corners=True).cpu().numpy()[0, 0]
 
             new_voxel_size = np.array((1.0, 1.0, 1.0))
-            print("Real voxel size: ", new_voxel_size)
-            print("New shape: ", new_data.shape)
+            # print("Real voxel size: ", new_voxel_size)
+            # print("New shape: ", new_data.shape)
 
             new_data = new_data.transpose((2, 1, 0))
 
@@ -54,9 +64,9 @@ def my_reform_1a(input_mrc, output_mrc, use_gpu=False):
                 vox_sizes.z = new_voxel_size[2]
                 mrc.voxel_size = vox_sizes
                 mrc.update_header_from_data()
-                mrc.header.nxstart = orig_map.header.nxstart * voxel_size[0]
-                mrc.header.nystart = orig_map.header.nystart * voxel_size[1]
-                mrc.header.nzstart = orig_map.header.nzstart * voxel_size[2]
+                mrc.header.nxstart = orig_map.header.nxstart * orig_voxel_size[0]
+                mrc.header.nystart = orig_map.header.nystart * orig_voxel_size[1]
+                mrc.header.nzstart = orig_map.header.nzstart * orig_voxel_size[2]
                 mrc.header.origin = orig_map.header.origin
                 mrc.header.mapc = orig_map.header.mapc
                 mrc.header.mapr = orig_map.header.mapr
--- data_processing/Unify_Map.py
+++ data_processing/Unify_Map.py
@@ -7,15 +7,14 @@ import numpy as np
 def Unify_Map(input_map_path, new_map_path):
     # Read MRC file
     mrc = mrcfile.open(input_map_path, permissive=True)
-
     data = mrc.data.copy()
     voxel_size = np.asarray(mrc.voxel_size.tolist(), dtype=np.float32)
     origin = np.array(mrc.header.origin.tolist(), dtype=np.float32)
     nstart = np.asarray([mrc.header.nxstart, mrc.header.nystart, mrc.header.nzstart], dtype=np.float32)
     cella = np.array(mrc.header.cella.tolist(), dtype=np.float32)
     mapcrs = np.asarray([mrc.header.mapc, mrc.header.mapr, mrc.header.maps], dtype=int)
-    if np.sum(nstart) == 0:
-        return input_map_path
+    # if np.sum(nstart) == 0:
+    #     return input_map_path
     mrc.print_header()
     mrc.close()
 
@@ -26,8 +25,9 @@ def Unify_Map(input_map_path, new_map_path):
     nstart = np.asarray([nstart[i] for i in sort])
     data = np.transpose(data, axes=2 - sort[::-1])
 
-    # Move offsets from nstart to origin
-    origin = origin + nstart * voxel_size
+    # Move offsets from nstart to origin if origin is zero (MRC2000)
+    if np.sum(origin) == 0:
+        origin = origin + nstart * voxel_size
 
     # Save the unified map
     mrc_new = mrcfile.new(new_map_path, data=data, overwrite=True)
@@ -43,7 +43,11 @@ def Unify_Map(input_map_path, new_map_path):
 
 
 if __name__ == "__main__":
-    input_map_path = Path('../example/21051.mrc')
-    new_map_path = Path('../example/21051_unified.mrc')
-    new_map_path = Unify_Map(input_map_path, new_map_path)
+    import argparse
+
+    args = argparse.ArgumentParser()
+    args.add_argument("-i", "--input_map_path", type=str, default=None)
+    args.add_argument("-o", "--output_map_path", type=str, default=None)
+    args = args.parse_args()
+    new_map_path = Unify_Map(args.input_map_path, args.output_map_path)
     print(f"New map path is {new_map_path}")
